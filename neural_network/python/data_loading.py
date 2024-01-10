423bbf74c0a07e9268fffac309efd7bfac7fada7
Ugnius <ugnius.alekna@gmail.com>
2024-01-10 19:24:49 +0200

    Updated Dataset object
    Removed everything associated with pressure;
    Added a functionality to load all data at once

--- neural_network/python/data_loading.py
+++ neural_network/python/data_loading.py
@@ -5,15 +5,14 @@ from torch.utils.data import Dataset, random_split
 import time
 
 class FlowFieldDataset(Dataset):
-    def __init__(self, velocity_dir, pressure_dir, load_all_data=False):
+    def __init__(self, velocity_dir, load_all_data=False):
         start_time = time.time()
         
         self.velocity_dir = velocity_dir
-        self.pressure_dir = pressure_dir
         self.load_all_data = load_all_data
 
-        self.files_true = self.get_joined_files('true_data')
-        self.files_noisy = self.get_joined_files('noisy_data')
+        self.files_true = sorted(os.listdir(os.path.join(self.velocity_dir, 'true_data')))
+        self.files_noisy = sorted(os.listdir(os.path.join(self.velocity_dir, 'noisy_data')))
 
         if self.load_all_data:
             self.data_true = [self.load_one_instance(f, 'true_data') for f in self.files_true]
@@ -22,44 +21,25 @@ class FlowFieldDataset(Dataset):
         self.load_time = time.time() - start_time
         print(f'Dataset Loaded in {self.load_time:.4f} s')
 
-    def load_one_instance(self, file_tuple, data_type):
-        u_file, p_file = file_tuple
-        u_data = np.load(os.path.join(self.velocity_dir, data_type, u_file))
-        p_data = np.load(os.path.join(self.pressure_dir, data_type, p_file))
-
-        return torch.from_numpy(u_data).float(), torch.from_numpy(p_data).float()
+    def load_one_instance(self, file_name, data_type):
+        u_data = np.load(os.path.join(self.velocity_dir, data_type, file_name))
+        return torch.from_numpy(u_data).float()
 
     def __len__(self):
         return len(self.files_true)
 
     def __getitem__(self, idx):
         if self.load_all_data:
-            u_hr_tensor, p_hr_tensor = self.data_true[idx]
-            u_lr_tensor, p_lr_tensor = self.data_noisy[idx]
+            u_hr_tensor = self.data_true[idx]
+            u_lr_tensor = self.data_noisy[idx]
         else:
-            u_hr_tensor, p_hr_tensor = self.load_one_instance(self.files_true[idx], 'true_data')
-            u_lr_tensor, p_lr_tensor = self.load_one_instance(self.files_noisy[idx], 'noisy_data')
+            u_hr_tensor = self.load_one_instance(self.files_true[idx], 'true_data')
+            u_lr_tensor = self.load_one_instance(self.files_noisy[idx], 'noisy_data')
 
         u_hr_tensor = u_hr_tensor.permute(2, 0, 1)
-        p_hr_tensor = p_hr_tensor.unsqueeze(0)
         u_lr_tensor = u_lr_tensor.permute(2, 0, 1)
-        p_lr_tensor = p_lr_tensor.unsqueeze(0)
 
-        return (u_hr_tensor, p_hr_tensor), (u_lr_tensor, p_lr_tensor)
-
-    def join_files(self, velocity_files, pressure_files):
-        joined_files = []
-        for u_file in velocity_files:
-            u_index = u_file.split('_')[-1].split('.')[0]
-            corresponding_p_file = next((p for p in pressure_files if p.split('_')[-1].split('.')[0] == u_index), None)
-            if corresponding_p_file:
-                joined_files.append((u_file, corresponding_p_file))
-        return joined_files
-
-    def get_joined_files(self, folder_name):
-        velocity_files = sorted(os.listdir(os.path.join(self.velocity_dir, folder_name)))
-        pressure_files = sorted(os.listdir(os.path.join(self.pressure_dir, folder_name)))
-        return self.join_files(velocity_files, pressure_files)
+        return u_hr_tensor, u_lr_tensor
 
 
 def train_test_split(dataset, test_size=0.2):
